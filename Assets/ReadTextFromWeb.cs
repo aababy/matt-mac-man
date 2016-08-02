f747f69dc736cb0c61df8bf30548b517b08c4b12
luoxiaopeng <luoxiaopeng@smalltreemedia.com>
2016-08-02 09:53:50 +0800

    no message

---
+++ Assets/ReadTextFromWeb.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ReadTextFromWeb : MonoBehaviour {
+	public string url = "http://www.ascii-art.de/ascii/ab/badger.txt";
+
+	// Use this for initialization
+	IEnumerator Start () {
+		Text textUI = GetComponent<Text> ();
+		textUI.text = "(loading file ...)";
+
+		WWW www = new WWW (url);
+		yield return www;
+		string textFileContents = www.text;
+		Debug.Log (textFileContents);
+
+		textUI.text = textFileContents;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
